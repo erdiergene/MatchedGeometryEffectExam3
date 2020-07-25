2aacd8615ce290f0c3adb273a516bfb0c0de57c3
erdiergene <erdi.ergene@ibtech.com.tr>
2020-07-26 01:12:26 +0300

    add example

---
+++ MatchedGeometryEffectExam3/MatchedGeometryEffectExam3App.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectExam3App.swift
+//  MatchedGeometryEffectExam3
+//
+//  Created by Erdi Ergene on 18.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectExam3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
